9282c269882e8e1523c1629c0f41e02e183ac4f3
Rayhan <rayhanmrizki21@gmail.com>
2023-11-12 21:33:35 +0700

    user seeder

--- database/seeders/UserSeed.php
+++ database/seeders/UserSeed.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,28 @@ class UserSeed extends Seeder
      */
     public function run(): void
     {
-        //
+        User::insert([
+        [
+            'user_id' => 'DK110001',
+            'user_name' => 'Rayhan M Rizki',
+            'role' => 'dokter',
+            'email' => 'rayhan@gmail.com',
+            'password' => bcrypt('12345678')
+        ],
+        [
+            'user_id' => 'MG000001',
+            'user_name' => 'Syakira',
+            'role' => 'manager',
+            'email' => 'Syakira@gmail.com',
+            'password' => bcrypt('12345678')
+        ],
+        [
+            'user_id' => 'AP100001',
+            'user_name' => 'Tarisa',
+            'role' => 'apoteker',
+            'email' => 'tarisa@gmail.com',
+            'password' => bcrypt('12345678')
+        ]
+        ]);
     }
 }
