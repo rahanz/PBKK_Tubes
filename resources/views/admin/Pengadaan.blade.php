3f4419a5d28b380cf1570d7a06f3ee24cf27501f
Rayhan <rayhanmrizki21@gmail.com>
2023-11-14 01:58:49 +0700

    halaman pengdaan obat

--- resources/views/admin/Pengadaan.blade.php
+++ resources/views/admin/Pengadaan.blade.php
@@ -3,50 +3,113 @@
 @section('PageAcces', 'Pengadaan Obat')
 
 @section('Pengadaan')
-    <div class="mt-1 my-4 p-4 bg-body rounded shadow-sm">
-        <h1 class="mb-4">Pengadaan Obat</h1>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="mt-1 my-3 p-4 bg-body rounded shadow-sm">
+                <h1 class="mb-4">Pengadaan Obat</h1>
 
-        <!-- Formulir Pengadaan Obat -->
-        <form id="formPengadaan">
-            <div class="form-group">
-                <label for="namaObat">Nama Obat:</label>
-                <input type="text" class="form-control" id="namaObat" required>
+                <!-- Form Tambah Data -->
+                <form method="POST" action="{{ route('InputPengadaan') }}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="NamaObat">Nama Obat :</label>
+                        <input type="text" class="form-control" name="NamaObat">
+                    </div>
+                    <div class="form-group">
+                        <label for="TanggalKadaluarsa">Tanggal Kadaluarsa :</label>
+                        <input type="date" class="form-control" name="TanggalKadaluarsa">
+                    </div>
+                    <div class="form-group">
+                        <label for="kategoriObat">Kategori :</label>
+                        <select class="form-control" name="KategoriObat">
+                            <option value="Obat Flu">Obat Flu</option>
+                            <option value="Obat Pencernaan">Obat Pencernaan</option>
+                            <option value="Obat Penurun Panas">Obat Penurun Panas</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="DeskripsiObat" class="form-label-lg">Deskripsi :</label>
+                        <textarea type="text" class="form-control" name="DeskripsiObat"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="stokObat">Stok Obat :</label>
+                        <input type="number" class="form-control" name="stokObat">
+                    </div>
+                    <div class="form-group">
+                        <label for="TanggalMasukObat">Tanggal Masuk :</label>
+                        <input type="date" class="form-control" name="Tanggal_Masuk">
+                    </div>
+                    <div class="form-group">
+                        <label for="PemasokObat">Pemasok :</label>
+                        <input type="text" class="form-control" name="PemasokObat">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Tambah</button>
+                </form>
             </div>
-            <div class="form-group">
-                <label for="jumlahObat">Jumlah Obat:</label>
-                <input type="number" class="form-control" id="jumlahObat" required>
-            </div>
-            <div class="form-group">
-                <label for="tanggalKadaluarsa">Tanggal Kadaluarsa:</label>
-                <input type="date" class="form-control" id="tanggalKadaluarsa" required>
-            </div>
-            <div class="form-group">
-                <label for="pemasok">Pemasok:</label>
-                <select class="form-control" id="pemasok" required>
-                    <option value="pemasok1">Pemasok 1</option>
-                    <option value="pemasok2">Pemasok 2</option>
-                    <!-- Tambahkan opsi pemasok lainnya sesuai kebutuhan -->
-                </select>
-            </div>
-            <button type="submit" class="btn btn-primary">Ajukan Pengadaan</button>
-        </form>
+        </div>
+    </section>
 
-        <!-- Tabel Riwayat Pengadaan -->
-        <table class="table mt-4">
-            <thead>
-                <tr>
-                    <th>Nomor Pengadaan</th>
-                    <th>Nama Obat</th>
-                    <th>Jumlah Obat</th>
-                    <th>Tanggal Kadaluarsa</th>
-                    <th>Pemasok</th>
-                    <th>Status</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody id="tabelRiwayatPengadaan">
-                <!-- Data riwayat pengadaan akan ditampilkan di sini -->
-            </tbody>
-        </table>
-    </div>
+    <!-- Tabel Data -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Tabel Data Pengadaan Obat</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="example1" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Nama Obat</th>
+                                        <th>Tanggal Kadaluarsa</th>
+                                        <th>Kategori</th>
+                                        <th>Deskripsi</th>
+                                        <th>Stok Obat</th>
+                                        <th>Tanggal Masuk</th>
+                                        <th>Pemasok</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($DataPengadaanObat as $dataPengadaan)
+                                        <tr>
+                                            <td>{{ $dataPengadaan->nama_obat }}</td>
+                                            <td>{{ $dataPengadaan->tanggal_kadaluarsa }}</td>
+                                            <td>{{ $dataPengadaan->kategori_obat }}</td>
+                                            <td>{{ $dataPengadaan->deskripsi_obat }}</td>
+                                            <td>{{ $dataPengadaan->stok_obat }}</td>
+                                            <td>{{ $dataPengadaan->tanggal_masuk_obat }}</td>
+                                            <td>{{ $dataPengadaan->pemasok_obat }}</td>
+                                            <td>
+                                                <button type="button" class="btn btn-block btn-warning btn-sm">Edit</button>
+                                                <button type="button" class="btn btn-block btn-danger btn-sm">Hapus</button>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th>Nama Obat</th>
+                                        <th>Tanggal Kadaluarsa</th>
+                                        <th>Kategori</th>
+                                        <th>Deskripsi</th>
+                                        <th>Stok Obat</th>
+                                        <th>Tanggal Masuk</th>
+                                        <th>Pemasok</th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- Data akan ditampilkan di sini -->
+                    </tbody>
+                    </table>
+                </div> <!-- cool-12 -->
+            </div> <!-- row -->
+        </div> <!-- container-fluid -->
+    </section>
 @endsection
