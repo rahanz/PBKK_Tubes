ec6ac1c284965ebb13437ca36d30d3ee13f13ae0
Rayhan <rayhanmrizki21@gmail.com>
2023-11-14 01:56:44 +0700

    migrasi table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('user_id')->unique();
+            $table->string('user_name');
+            $table->enum('role',['dokter','apoteker','manager']);
+            $table->string('email')->nullable()->unique();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 
