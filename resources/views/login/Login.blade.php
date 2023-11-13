2c634737704bd1fbf03a8efd2afc9fc199979f0d
Rayhan <rayhanmrizki21@gmail.com>
2023-11-14 01:59:45 +0700

    halaman login

--- resources/views/login/Login.blade.php
+++ resources/views/login/Login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login/Register Page</title>
+    <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
@@ -15,16 +15,17 @@
             <div class="col-md-6">
                 <div class="card p-4 border rounded shadow border-primary">
                     <h2 class="mb-4 text-center text-primary">Login</h2>
-                    <form action="login.php" method="post">
+                    <form action="{{ route('login') }}" method="post">
+                        @csrf
                         <div class="form-group">
-                            <label for="username">Username:</label>
-                            <input type="text" class="form-control" id="username" name="username" required>
+                            <label for="UserName">Username :</label>
+                            <input type="text" class="form-control" name="User_Name">
                         </div>
                         <div class="form-group">
-                            <label for="password">Password:</label>
-                            <input type="password" class="form-control" id="password" name="password" required>
+                            <label for="password">Password :</label>
+                            <input type="password" class="form-control" name="password">
                         </div>
-                        <button type="submit" class="btn btn-primary btn-block">Login</button>
+                        <button type="submit" class="btn btn-primary btn-block mt-3">Login</button>
                     </form>
                 </div>
             </div>
